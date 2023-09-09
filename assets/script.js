aa8b5d49f3b3e1ce988a5d7e745a4906c1bf4271
DillenGrant <dillen.grant72@gmail.com>
2023-09-09 12:24:22 -0600

    char comfirm

--- assets/script.js
+++ assets/script.js
@@ -10,10 +10,19 @@ function generatePassword(){
   var length = parseInt(prompt("Enter the length of the password (8-128 characters):"));
   if (isNaN(length) || length < 8 || length > 128) {
       alert("Please enter a valid password length.");
-      return getPasswordCriteria();
+      return generatePassword();
   }
-}
 
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("At least one character type must be selected.");
+    return generatePassword();
+ }
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
